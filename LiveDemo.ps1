b2e1ee0e74ac98b7bf2182ca0a4a46b53fada322
Ryan Leap <ryan.leap@gmail.com>
2019-09-29 10:56:31 -0400

    Organizing demo for GitHub publishing

---
+++ LiveDemo.ps1
@@ -0,0 +1,64 @@
+<#
+.Synopsis
+   Obtain disk usage reporter (uses du.exe from Sysinternals)
+   https://docs.microsoft.com/en-us/sysinternals/downloads/sysinternals-suite 
+.DESCRIPTION
+   Demo wrapper code using Sysinternals Disk Usage Tool.  Demonstrates PowerShell's powers of amplification:
+
+   Predictablitiy - Verb-Noun naming
+   Discoverability - Built-in help w/Examples
+   Composability - Pipeline support
+.PARAMETER Path
+   Specifies path to be checked for disk usage
+.EXAMPLE
+   Get-ChildItem C:\scripts\ -Directory | Get-SysInternalsDiskUsage
+.EXAMPLE
+   Get-ChildItem -Path (Join-Path -Path $env:USERPROFILE -ChildPath Downloads) -Directory -Recurse | Get-SysInternalsDiskUsage | Sort-Object -Property DirectorySize
+.NOTES
+   Written during (and tweaked after) PoweShell Saturday RTPSUG (https://rtpsug.com/pssaturday) talk
+   'Why Can't We Be Friends: PowerShell & Command Line Utilities'
+   
+   Author/Speaker:
+   - Ryan Leap
+   - GitHub: https://github.com/ryan-leap
+   - Twitter: @leap_ryan
+#>
+function Get-SysInternalsDiskUsage
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([pscustomobject])]
+    Param
+    (
+        # Path to directory
+        [ValidateScript({Test-path -Path $_ -PathType Container})]
+        [Alias('FullName')]
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [string] $Path
+
+    )
+
+    Begin
+    {
+        Write-Verbose "Begin block"
+    }
+    Process
+    {
+        Write-Verbose "Process block"
+        $result = & C:\Tools\SysInternalsSuite\du.exe -nobanner $Path 2>$null
+        [pscustomobject] @{
+            'Path'                = $Path
+            'FileCount'           = [int64] $result[0].Substring('Files:        '.Length)
+            'DirectoryCount'      = [int64] $result[1].Substring('Directories:  '.Length)
+            'DirectorySize'       = [int64] $result[2].Substring(0,$result[2].IndexOf(' bytes')).Replace(',','').Substring('Size:         '.Length)
+            'DirectorySizeOnDisk' = [int64] $result[3].Substring(0,$result[2].IndexOf(' bytes')).Replace(',','').Substring('Size on disk: '.Length)
+         }
+         
+    }
+    End
+    {
+        Write-Verbose "End block"
+    }
+}
--- Three_Design_Principles.ps1
+++ Three_Design_Principles.ps1
@@ -9,30 +9,10 @@ Get-Command -Verb Stop
 # Not Kill, Not Abort, not End, Stop.
 # Stop a Service, Stop a Process.  Predictable
 Get-Command | Group-Object -Property Verb
+Get-Command -CommandType Cmdlet | Group-Object -Property Verb | Sort-Object -Property Count
 
 # Parameter names are also predictable
 Get-Command -ParameterName Credential
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 <#
@@ -47,30 +27,14 @@ Get-Help about_*
 
 
 
-
-
-
-
-
-
-
 <#
 
    Composable
 
 #>
 import-csv C:\scripts\PsSaturday\flavors.csv
-import-csv C:\scripts.PsSaturday\flavors | Group-Object -Property flavors
-# where name like berry
-# select name and count
-# sort by count
-# Why composability is possible?  Because in PowerShell results are
-# objects and object properties can be mapped into the next cmdlet and so on
-
-
-
-
-
+import-csv C:\scripts.PsSaturday\flavors | Where-Object -Property Flavor -Like '*berry*' |
+Group-Object -Property flavors | Sort-Object -Property Count
 
 
 <#
@@ -78,69 +42,24 @@ import-csv C:\scripts.PsSaturday\flavors | Group-Object -Property flavors
    Parsing Output - The Easy Way (because the utility provides CSV)
 
 #>
-ConvertFrom-Csv
 
 & C:\Tools\SysInternalsSuite\du.exe /?
-& C:\Tools\SysInternalsSuite\du.exe -c C:\AutoLab\
-& C:\Tools\SysInternalsSuite\du.exe -c C:\AutoLab\ -nobanner
-& C:\Tools\SysInternalsSuite\du.exe -c C:\AutoLab\ -nobanner | ConvertFrom-Csv
-& C:\Tools\SysInternalsSuite\du.exe -c C:\AutoLab\ -nobanner | ConvertFrom-Csv | get-member
+& C:\Tools\SysInternalsSuite\du.exe -nobanner C:\AutoLab\
 $result = & C:\Tools\SysInternalsSuite\du.exe -c C:\AutoLab\ -nobanner | ConvertFrom-Csv
-$result
-$result.CurrentFileCount + 10
-$result.CurrentFileCount.GetType().Name
-$result.DirectorySize -is [string]
-([int] $result.FileCount) + 3
-$result | Select-Object -Property Path,@{Name='FileCount'; Expression={[int]$_.FileCount}}
-$modifiedResult = $result | Select-Object -Property Path,@{Name='FileCount'; Expression={$_.FileCount}}
-$modifiedResult.FileCount + 3
-$modifiedResult = $result | Select-Object -Property Path,@{Name='FileCount'; Expression={[int]$_.FileCount}}
-$modifiedResult.FileCount.GetType().Name
-$modifiedResult.FileCount + 3
-$modifiedResult = $result | Select-Object -Property Path,@{Name='FileCount'; Expression={[int]$_.FileCount}},@{Name='DirectoryCount'; Expression={[int]$_.DirectoryCount}}
-
-$modifiedResult = $result | Select-Object -Property Path,
-    @{Name='CurrentFileCount'; Expression={[int]$_.CurrentFileCount}},
-    @{Name='CurrentFileSize'; Expression={[int]$_.CurrentFileSize}},
-    @{Name='FileCount'; Expression={[int]$_.FileCount}},
-    @{Name='DirectoryCount'; Expression={[int]$_.DirectoryCount}},
-    @{Name='DirectorySize'; Expression={[int]$_.DirectorySize}},
-    @{Name='DirectorySizeOnDisk'; Expression={[int]$_.DirectorySizeOnDisk}}
-# Or...
-$result.CurrentFileCount = [int] $result.CurrentFileCount
-$result.CurrentFileSize = [int] $result.CurrentFileSize
 
 <#
 
     Parsing Output - the hard way (Screen scraping)
 
 #>
-
-& C:\Tools\SysInternalsSuite\du.exe /?
-& C:\Tools\SysInternalsSuite\du.exe C:\AutoLab\
-& C:\Tools\SysInternalsSuite\du.exe C:\AutoLab\ -nobanner
-$result = & C:\Tools\SysInternalsSuite\du.exe C:\AutoLab\ -nobanner
 $result = & C:\Tools\SysInternalsSuite\du.exe C:\AutoLab\ -nobanner 2>$null
 get-help about_Redirection
-$result = & C:\Tools\SysInternalsSuite\du.exe -nobanner C:\AutoLab\ 2>$null
-$result[0]
-$result[0] | get-member
-$result[0]
-$result[1]
-$result[2]
-$result
-$result[-1]
-$result[-2]
-$result[0].Substring(5)
-$result[0].Substring('Files:        '.Length)
-$result[1].Substring('Directories:  '.Length)
-$result[2].Substring('Size:         '.Length)
-$result
 $result[2].IndexOf(' bytes')
 $result[2].Substring(0,$result[2].IndexOf(' bytes'))
 $result[2].Substring(0,$result[2].IndexOf(' bytes')).Replace(',','')
 $result[2].Substring(0,$result[2].IndexOf(' bytes')).Replace(',','').Substring('Size:         '.Length)
 [int] $result[2].Substring(0,$result[2].IndexOf(' bytes')).Replace(',','').Substring('Size:         '.Length)+ 7
+
 
 <#
 
@@ -149,10 +68,14 @@ $result[2].Substring(0,$result[2].IndexOf(' bytes')).Replace(',','').Substring('
     HashTable --> PS Custom Object
 
 #>
-$result = & C:\Tools\SysInternalsSuite\du.exe -nobanner $Path
+$result = & C:\Tools\SysInternalsSuite\du.exe -nobanner $Path 2>$null
 [pscustomobject] @{
    'FileCount'           = [int64] $result[0].Substring('Files:        '.Length)
    'DirectoryCount'      = [int64] $result[1].Substring('Directories:  '.Length)
    'DirectorySize'       = [int64] $result[2].Substring(0,$result[2].IndexOf(' bytes')).Replace(',','').Substring('Size:         '.Length)
    'DirectorySizeOnDisk' = [int64] $result[3].Substring(0,$result[2].IndexOf(' bytes')).Replace(',','').Substring('Size on disk: '.Length)
 }
+
+# Advanced Function
+
+# Cut & Paste from ISE snippet
